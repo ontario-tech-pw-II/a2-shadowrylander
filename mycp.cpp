60bd1697c50a89fe295ca031246e807d086cc247
shadowrylander <titaniumfiles@outlook.com>
2020-01-17 09:03:47 -0500

    init

--- mycp.cpp
+++ mycp.cpp
@@ -1,13 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
  	ifstream fin;
  	ofstream fout;
-	
+
 	// check if there are enough arguments
-	
-	
+  if (argc < 3) {
+    cerr << "Sorry! You haven't provided enough arguments!" << endl;
+    return 1;
+  } else if (argc == 3) {
+    
+  } else {
+    cerr << "Sorry! You have provided too many arguments!" << endl;
+    return 1;
+  }
+
 	// open the first file
- 	
+  fin.open(argv[1]);
+
 	char c;
 
  	if (fin.fail()) // check if it is successful 
@@ -15,25 +29,26 @@ int main(int argc, char const *argv[])
  		cerr << " Cannot open the input file!" << endl;
  		return 1;
  	}
- 	
+
 
 	// open the second file
-	
+  fout.open(argv[2]);
+
  	if (fout.fail())
  	{
  		cerr << " Cannot open the output file!" << endl;
  		return 1;
  	}
- 	
+
  	while(fin.get(c)) 
 	{
 		fout << c;
 	}
- 	
- 	fin.close(); 
+
+ 	fin.close();
 
  	fout.close();
 
  	 return 0;
 
- } 
+}
